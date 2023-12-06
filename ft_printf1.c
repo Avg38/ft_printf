00392a448fe662f70fce10ec7d18c386885e8053
Avg38 <92724863+Avg38@users.noreply.github.com>
2023-12-06 16:59:58 +0100

    finish

--- ft_printf1.c
+++ ft_printf1.c
@@ -6,76 +6,57 @@
 /*   By: avialle- <avialle-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 14:36:42 by avialle-          #+#    #+#             */
-/*   Updated: 2023/12/06 14:22:49 by avialle-         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:58:13 by avialle-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-int	ft_print_char(int c)
+void	ft_print_char(int c, size_t *len)
 {
 	write(1, &c, 1);
-	return (1);
+	(*len)++;
 }
 
-int	ft_print_str(char *str)
+void	ft_print_str(char *str, size_t *len)
 {
-	int	i;
+	size_t	i;
 
 	if (!str)
-	{
-		ft_print_str("(null)");
-		return (6);
-	}
+		return (ft_print_str("(null)", len));
 	i = 0;
 	while (str[i])
 	{
-		ft_print_char(str[i]);
+		ft_print_char(str[i], len);
 		i++;
 	}
-	return (ft_strlen(str));
 }
 
-static int	len_var(va_list args, const char c)
+static void	len_var(va_list args, const char c, size_t *len)
 {
-	int	len;
-
-	len = 0;
 	if (c == 'c')
-		len = ft_print_char(va_arg(args, int));
+		ft_print_char(va_arg(args, int), len);
 	else if (c == 's')
-		len = ft_print_str(va_arg(args, char *));
+		ft_print_str(va_arg(args, char *), len);
 	else if (c == 'p')
-		len = ft_print_ptr(va_arg(args, void *));
+		ft_print_ptr(va_arg(args, unsigned long long), len);
 	else if (c == 'd' || c == 'i')
-		ft_print_nbr(va_arg(args, int), &len);
+		ft_print_nbr(va_arg(args, int), len);
 	else if (c == 'u')
-		ft_print_unsigned(va_arg(args, unsigned int), &len);
+		ft_print_unsigned(va_arg(args, unsigned int), len);
 	else if (c == 'x')
-		ft_print_hexa(va_arg(args, unsigned int), "0123456789abcdef", &len);
+		ft_print_hexa(va_arg(args, unsigned int), "0123456789abcdef", len);
 	else if (c == 'X')
-		ft_print_hexa(va_arg(args, unsigned int), "0123456789ABCDEF", &len);
+		ft_print_hexa(va_arg(args, unsigned int), "0123456789ABCDEF", len);
 	else if (c == '%')
-		len = ft_print_char(37);
-	printf("   %d", len);
-	return (len);
+		ft_print_char(37, len);
 }
 
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
-	int		i;
-	int		len;
+	size_t	i;
+	size_t	len;
 
 	va_start(args, str);
 	i = 0;
@@ -84,25 +65,22 @@ int	ft_printf(const char *str, ...)
 	{
 		if (str[i] == '%')
 		{
-			len += len_var(args, str[i + 1]);
+			len_var(args, str[i + 1], &len);
 			i++;
 		}
 		else
-		{
-			ft_print_char(str[i]);
-			len++;
-		}
+			ft_print_char(str[i], &len);
 		i++;
 	}
 	va_end(args);
-	return (len);
+	return ((int)len);
 }
 
 // int	main(void)
 // {
 // 	char	c = 'c';
 // 	char	*str = NULL;
-// 	char	*ptr = NULL;
+// 	char	*ptr = "NULL";
 // 	unsigned int	d = 2147483699;
 // 	int		i = 2147483647;
 // 	unsigned int	u = 2147483699;
@@ -110,8 +88,6 @@ int	ft_printf(const char *str, ...)
 // 	int		X = -365;
 
 // 	// printf("%s\n", str);
-// 	ft_printf("ft_printf:\n%c\n%s\n%p\n%d
-// 	\n%i\n%u\n%x\n%X\n%%\n\n", c, str, ptr, d, i, u, x, X);
-// 	printf("\nprintf:\n%c\n%s\n%p\n%d
-// 	\n%i\n%u\n%x\n%X\n%%\n", c, str, ptr, d, i, u, x, X);
+// 	printf("%d", ft_printf("%c\n%s\n%p\n%d\n%i\n%u\n%x\n%X\n", c, str, ptr, d, i, u, x, X));
+// 	// printf("%d", printf("%c%s%p%d%i%u%x%X%z%", c, str, ptr, d, i, u, x, X));
 // }
